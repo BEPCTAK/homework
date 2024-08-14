1cd96f82b1b17721f7b57f01b54bda1d0f3b36e1
BEPCTAK <bltinsyf@gmail.com>
2024-08-14 17:40:59 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+#  Организуйте программу:
+#
+#     Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
+#     Вывести количество символов введённого текста
+my_string = input("Напишите любое словосочетание: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
