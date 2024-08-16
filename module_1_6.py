b7b0b6e65839f99e0b79745a72291be23daa7bf4
BEPCTAK <bltinsyf@gmail.com>
2024-08-16 21:26:59 +0700

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,12 +1,11 @@
-my_dict = {"john": 2001, "Anna": 1998, "Mike": 2003}
+my_dict = {"John": 2001, "Anna": 1998, "Mike": 2003}
 print(my_dict)
 print(my_dict["Anna"])
 my_dict["Nikolay"]= 1999
 print(my_dict["Nikolay"])
 my_dict.update({"Peter": 2010, "Olga": 2000})
-print(my_dict)
-del my_dict["Mike"]
-print(my_dict.get("Mike"))
+a = my_dict.pop("John")
+print(a)
 print(my_dict)
 my_set = {1, 2, 1, 2, 3, "pen", "pencil", "table", (4, 17, 3, 11)}
 print(my_set)
