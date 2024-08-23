e334fe7cd8160f556f06a515107470390d8f3d69
Любопытный <bltinsyf@gmail.com>
2024-08-23 18:51:33 +0700

    12,08

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    element = my_list[index]
+    index += 1
+    if element >= 1:
+        print(element)
+    elif element < 0:
+        break
+    
+
