f2ee65f5acb0338969121fede278b99143d95597
BEPCTAK <bltinsyf@gmail.com>
2024-09-08 00:29:12 +0700

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,16 @@
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if "@" not in recipient or "@" not in sender or not (
+            recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net'))):
+        print(f"Невозможно отправить письмо с адреcа {sender} на адреc {recipient}")
+        return
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', 'urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', 'urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', 'urban.teacher@mail.ru')
