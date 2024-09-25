bb29666aa822a8c8a3d7be84aa6451734c4b5732
BEPCTAK <bltinsyf@gmail.com>
2024-09-26 03:04:32 +0700

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -48,6 +48,10 @@ class House:
         if isinstance(value, int):
             self.number_of_floors += value
             return self
+        elif isinstance(value, House):
+
+            self.number_of_floors += value.number_of_floors
+            return self
         return NotImplemented
 
     def __radd__(self, value):
@@ -59,24 +63,18 @@ class House:
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
-
 print(h1)
 print(h2)
-
-print(h1 == h2)  # __eq__ проверка на равенство
-
-h1 = h1 + 10  # __add__ оператора сложения (+)
+print(h1 == h2)
+h1 = h1 + 10
 print(h1)
 print(h1 == h2)
-
-h1 += 10  # __iadd__
+h1 += 10
 print(h1)
-
-h2 = 10 + h2  # __radd__
+h2 = 10 + h2
 print(h2)
-
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
