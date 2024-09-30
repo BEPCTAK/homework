f2f8cd6bf0d886ab811c22cb42d415cd0d67996d
BEPCTAK <bltinsyf@gmail.com>
2024-09-30 22:19:10 +0700

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -5,10 +5,9 @@ class House:
 
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
-            for floor in range(1, new_floor + 1):
-                print(floor)
-        else:
             print("Такого этажа не существует")
+        else:
+            print(f"Вы на этаже {new_floor}")
 
     def __len__(self):
         return self.number_of_floors
@@ -21,15 +20,17 @@ class House:
             return self.number_of_floors == other.number_of_floors
         elif isinstance(other, int):
             return self.number_of_floors == other
+        return NotImplemented
 
-        def __ne__(self, other):
-            return not self.__eq__(other)
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __lt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
         elif isinstance(other, int):
             return self.number_of_floors < other
+        return NotImplemented
 
     def __le__(self, other):
         return self.__eq__(other) or self.__lt__(other)
@@ -45,6 +46,7 @@ class House:
             return House(self.name, self.number_of_floors + other.number_of_floors)
         elif isinstance(other, int):
             return House(self.name, self.number_of_floors + other)
+        return NotImplemented
 
     def __radd__(self, other):
         return self.__add__(other)
