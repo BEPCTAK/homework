c9f0c5897e63f6e731e2c9fd020ea2b149690de3
BEPCTAK <bltinsyf@gmail.com>
2024-11-12 15:00:30 +0700

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,32 @@
+import time
+import multiprocessing
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:  # Если строка пустая, выходим из цикла
+                break
+            all_data.append(line.strip())  # Добавляем строку в список, убирая лишние пробелы
+    # Возвращать или выводить список all_data не нужно, просто собираем данные
+
+
+if __name__ == '__main__':
+    # Список названий файлов
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    linear_duration = time.time() - start_time
+    print(f'Линейное выполнение: {linear_duration:.6f} секунд')
+
+    # Многопроцессный вызов
+    start_time = time.time()
+    with multiprocessing.Pool() as pool:
+        pool.map(read_info, filenames)
+    multiprocessing_duration = time.time() - start_time
+    print(f'Многопроцессное выполнение: {multiprocessing_duration:.6f} секунд')
