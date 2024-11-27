41f0bc3facf84318b8085a5ca3a51224a18d001b
BEPCTAK <bltinsyf@gmail.com>
2024-11-27 14:31:50 +0700

    Add files via upload

---
+++ module_11_3.py
@@ -0,0 +1,46 @@
+def introspection_info(obj):
+    """Функция для интроспекции объекта и получения его информации."""
+    info = {}
+
+    # Определяем тип объекта
+    info['type'] = type(obj).__name__
+
+    # Получаем атрибуты объекта
+    info['attributes'] = dir(obj)
+
+    # Получаем методы объекта
+    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
+    info['methods'] = methods
+
+    # Получаем модуль, к которому принадлежит объект
+    info['module'] = getattr(obj, '__module__', None)  # Используем getattr для избежания ошибки
+
+    # Другие интересные свойства объекта
+    info['is_instance'] = isinstance(obj, object)
+
+    # Если объект является классом, добавим информацию о его родительских классах
+    if isinstance(obj, type):
+        info['base_classes'] = obj.__bases__
+
+    return info
+
+
+# Примеры работы функции
+number_info = introspection_info(42)
+print(number_info)
+
+string_info = introspection_info("Hello, World!")
+print(string_info)
+
+list_info = introspection_info([1, 2, 3])
+print(list_info)
+
+
+# Пример для пользовательского класса
+class MyClass:
+    def my_method(self):
+        pass
+
+
+my_class_info = introspection_info(MyClass)
+print(my_class_info)
